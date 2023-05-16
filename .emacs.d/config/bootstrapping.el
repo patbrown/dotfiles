6761b97b0382ca1829862f4161e191386a679df7
Patrick Brown <contactpatbrown@gmail.com>
2023-05-16 13:15:29 -0500

    Removing last semblance of straight.el

--- .emacs.d/config/bootstrapping.el
+++ .emacs.d/config/bootstrapping.el
@@ -27,21 +27,21 @@
 ;;     (server-start)))
 
 ;; Bootstrapping STRAIGHT.EL for functional package management
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
+;; (defvar bootstrap-version)
+;; (let ((bootstrap-file
+;;        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+;;       (bootstrap-version 5))
+;;   (unless (file-exists-p bootstrap-file)
+;;     (with-current-buffer
+;;         (url-retrieve-synchronously
+;;          "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+;;          'silent 'inhibit-cookies)
+;;       (goto-char (point-max))
+;;       (eval-print-last-sexp)))
+;;   (load bootstrap-file nil 'nomessage))
 
 ;; Use package for convenience.
-(straight-use-package 'use-package)
+;(straight-use-package 'use-package)
 (require 'use-package)
 (setq use-package-always-ensure t)
 
